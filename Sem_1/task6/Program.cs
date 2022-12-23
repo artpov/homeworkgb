36d8724621d6d825ee73024c6915ea074dba75bd
Artyom <povarov.artem5@yandex.ru>
2022-12-23 19:20:02 +0300

    hm1

---
+++ Sem_1/task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if (Num % 2 == 1)
+{
+    Console.WriteLine("Число" + Num + " является: НЕЧЁТНЫМ");
+}
+else
+{
+    Console.WriteLine("Число " + Num + "является: ЧЁТНЫМ");
+}
