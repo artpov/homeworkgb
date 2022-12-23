ee7db7ed3e3733ddc0f8f55b13f7d74f65c4bff0
Artyom <povarov.artem5@yandex.ru>
2022-12-23 19:25:29 +0300

    hm2

---
+++ Sem_2/task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write(" Введите трёхзначное число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+if ((num1 >= 100) && (num1 < 1000))
+{  
+    int firstDigigt = num1 % 100;
+    int secondDigit = firstDigigt / 10;
+    Console.Write(secondDigit);
+}
+else
+{
+    Console.Write("Введено не трёхзначное число ");
+}
