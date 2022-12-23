4c9f1a7eb9b7c5bab0a5e905a0185c9119dedda6
Artyom <povarov.artem5@yandex.ru>
2022-12-23 19:35:42 +0300

    hm6

---
+++ Sem_6/task41/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.WriteLine("Сколько чисел вы хотите ввести");
+int num = int.Parse(Console.ReadLine()!);
+
+int[] arr = new int[num];
+Console.WriteLine($"--> {АmountNumber(UserFillArray(arr))}");
+
+int[] UserFillArray(int[] array)
+{
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число");
+        arr[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int АmountNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
