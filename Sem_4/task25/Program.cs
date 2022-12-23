408b91f690d9e9ba4351433cb671c7f800b2a02f
Artyom <povarov.artem5@yandex.ru>
2022-12-23 19:30:45 +0300

    hm4

---
+++ Sem_4/task25/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("A=");
+int numA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("B=");
+int numB = int.Parse(Console.ReadLine()!);
+
+int expfunction = power(numA, numB);
+Console.WriteLine(expfunction);
+
+int power(int numberA, int numberB)
+{
+    int expAinB = numberA;
+    for (int i = 1; i<numberB;i++)
+{
+    expAinB = expAinB * numberA;
+}
+return expAinB;
+}
---
+++ Sem_4/task27/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+
+int digitFunction = Sumdigit(num);
+Console.WriteLine(digitFunction);
+
+int Sumdigit(int number)
+{
+    int sum = 0;
+    while (number>=10)
+    {
+        sum += number %10;
+        number /= 10;
+    }
+    return sum + number;
+}
