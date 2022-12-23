8fb334993f2411aa76e724cf289e8ef13b94f3ce
Artyom <povarov.artem5@yandex.ru>
2022-12-23 19:28:26 +0300

    hm3

---
+++ Sem_3/task19/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.Write("Введите число");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+    if (number [0] == number [4] || number [1] == number [3]){
+        Console.WriteLine($"Ваше число: {number} - да");
+    }
+    else Console.WriteLine($"Ваше число: {number} - нет");
+}
+
+if (number!.Length == 5){
+    CheckingNumber(number);
+}
+else Console.WriteLine("Введите правильное число");
